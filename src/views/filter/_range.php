443027a539a5a06c0e14930f893a2837a699ed0c
Denis <krolik@yandex.ru>
2018-05-07 16:57:38 +0300

    Update _range.php

--- src/views/filter/_range.php
+++ src/views/filter/_range.php
@@ -11,25 +11,24 @@ if ($parameter->min !== null && $parameter->max !== null) {
 }
 
 $hide = false;
-$inputId = str_replace(['[', ']'], '-', $parameter->getInputName());
 ?>
 
 <div class="list-filter-form-range"><!-- range -->
     <div class="input-group">
         <div class="input-group-addon">от</div>
-        <?= Html::input('number', "{$parameter->getInputName()}[from]", $parameter->getSelections()['from'] ?? $parameter->min, $options + ['id' => "{$widgetId}_{$inputId}-from", 'class' => "form-control {$inputId}-from"]) ?>
+        <?= Html::input('number', "{$parameter->getInputName()}[from]", $parameter->getSelections()['from'] ?? $parameter->min, $options + ['id' => "{$widgetId}_{$parameter->getId()}-from", 'class' => "form-control {$parameter->getId()}-from"]) ?>
         <div class="input-group-addon hidden-sm"><?= $parameter->unit ?></div>
     </div>
     <div class="input-group">
         <div class="input-group-addon">до</div>
-        <?= Html::input('number', "{$parameter->getInputName()}[to]", $parameter->getSelections()['to'] ?? $parameter->max, $options + ['id' => "{$widgetId}_{$inputId}-to", 'class' => "form-control {$inputId}-to"]) ?>
+        <?= Html::input('number', "{$parameter->getInputName()}[to]", $parameter->getSelections()['to'] ?? $parameter->max, $options + ['id' => "{$widgetId}_{$parameter->getId()}-to", 'class' => "form-control {$parameter->getId()}-to"]) ?>
         <div class="input-group-addon hidden-sm"><?= $parameter->unit ?></div>
     </div>
     <div class="list-filter-form-range-unit input-group-addon visible-sm-* hidden-xs hidden-md hidden-lg"><?= $parameter->unit ?></div>
 </div><!-- // range -->
 <?php if ($linkLabels && $parameter->getValues()): ?>
-    <?= Html::a('Все значения <i class="fa fa-angle-down"></i>', "#{$widgetId}_list-filter-form-all_{$inputId}", ['data-toggle' => 'collapse', 'class' => 'small list-filter-form-all-values-button']) ?>
-    <div id="<?= $widgetId ?>_list-filter-form-all_<?= $inputId ?>" class="panel-collapse collapse list-filter-form-all-values">
+    <?= Html::a('Все значения <i class="fa fa-angle-down"></i>', "#{$widgetId}_list-filter-form-all_{$parameter->getId()}", ['data-toggle' => 'collapse', 'class' => 'small list-filter-form-all-values-button']) ?>
+    <div id="<?= $widgetId ?>_list-filter-form-all_<?= $parameter->getId() ?>" class="panel-collapse collapse list-filter-form-all-values">
         <?php foreach ($parameter->getValues(true) as $value): ?>
             <div>
                 <?= Html::a("{$value->name} $parameter->unit", $value->url, ['title' => $value->title, 'class' => 'small']) ?>
